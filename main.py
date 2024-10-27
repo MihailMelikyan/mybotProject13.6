0ba77de9e366ec6238595c1dc28667bb1a81d911
MihailMelikyan <vip.mizhgan62@gmail.com>
2024-10-27 14:50:23 +0300

    при нажатии на инлайн клавиатуру 'Рассчитать норму калорий' не переходит в машину состояний

---
+++ main.py
@@ -0,0 +1,81 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+
+api = '7701233404:AAFMRvBKUoY2KKK1m3KGz7PoJeuVZR3P6yE'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup()
+button = KeyboardButton('Рассчитать')
+button2 = KeyboardButton('Информация')
+kb.add(button, button2)
+kb_in = InlineKeyboardMarkup()
+button3 = InlineKeyboardButton(text='Рассчитать норму калорий', callback_data='Calories')
+button4 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+kb_in.add(button3, button4)
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Приветствуем в нашем боте!', reply_markup=kb)
+
+
+@dp.message_handler(text='Информация')
+async def info(message):
+    await message.answer('Бот поможет следить за калориями v.1.0', reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:', reply_markup=kb_in)
+
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('10 х вес (кг) + 6,25 x рост (см) – 5 х возраст (г) + 5')
+    await call.answer()
+
+@dp.message_handler(text='Информация')
+async def info(message):
+    await message.answer('Бот поможет следить за калориями v.1.0', reply_markup=kb)
+
+
+@dp.callback_query_handler(text='Calories')
+async def set_age(call):
+    await call.message.answer('Привет я бот помощник и я помогу тебе считать калории!')
+    await call.answer()
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_growth(message):
+    await message.answer('Введите свой возраст:', reply_markup=kb)
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес:')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    calorie_norm = 10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age'])
+    await message.answer(f'Ваша норма калорий {calorie_norm}')
+    await state.finish()
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
